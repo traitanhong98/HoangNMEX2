a8c8890d79e012bd0221461ac514ec50c599dcb0
HoangNM <hoangnm@ttc-solutions.com>
2020-11-12 16:59:58 +0700

    Initial Commit

---
+++ NguyenMinhHoangEx02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NguyenMinhHoangEx02
+//
+//  Created by Hoang on 11/12/20.
+//  Copyright © 2020 Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
